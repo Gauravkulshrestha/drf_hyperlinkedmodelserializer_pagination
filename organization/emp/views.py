1f23390b9cec91e8b821e51f5cf0ea84487a0e98
Gaurav Kulshrestha <gauravkulshrestha757@gmail.com>
2022-03-07 18:14:30 +0530

    Added search feature.

--- organization/emp/views.py
+++ organization/emp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from .models import Employee
 from .serializers import EmployeeSerializer
 from rest_framework import viewsets
+from rest_framework import filters
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'id']
--- organization/organization/settings.py
+++ organization/organization/settings.py
@@ -40,11 +40,16 @@ INSTALLED_APPS = [
 
     'emp.apps.EmpConfig',
     'rest_framework',
+    'django_filters',
 ]
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 2
+# }
+
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 MIDDLEWARE = [
